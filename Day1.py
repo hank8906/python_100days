7f6e1e160d5f3592f5fa33879cdbb22f4c10af9b
hank <huanghank2000@gmail.com>
2023-08-05 18:18:41 +0800

    Day1

--- Day1.py
+++ Day1.py
@@ -1,1 +1,4 @@
-print("hello")
+print("welocome to the band name generator")
+city = input("what's the name of the city you grown up?")
+pet = input ("what's your pet's name?")
+print ("your band name could be " + city + pet)
