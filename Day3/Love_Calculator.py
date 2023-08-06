494019ee120eb22e1d8dfd380b0b5c34709e1d00
hank <huanghank2000@gmail.com>
2023-08-06 18:23:09 +0800

    Day3-Love Calculator

---
+++ Day3/Love_Calculator.py
@@ -0,0 +1,28 @@
+#Take both people's names and check for the number of times the letters in the word "TRUE" occurs. 
+#Then check for the number of times the letters in the word "LOVE" occurs.
+#Then combine these numbers to make a 2 digit number.
+#-------------------------------------------------------------------------------------
+#For Love Scores less than 10 or greater than 90, the message should be:
+# "Your score is **x**, you go together like coke and mentos."
+
+#For Love Scores between 40 and 50, the message should be:
+# "Your score is **y**, you are alright together.
+
+#"Otherwise, the message will just be their score. e.g.:
+# "Your score is **z**."
+#-------------------------------------------------------------------------------------
+# 1.Check the times that "TRUE" and "LOVE" occur in both name
+# 2.Combine the two number into 2 digits
+# 3.check the zone of the number in and print the answer
+#-------------------------------------------------------------------------------------
+# 🚨 Don't change the code below 👇
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+count1 = 0 
+count2 = 0
+
+ 
