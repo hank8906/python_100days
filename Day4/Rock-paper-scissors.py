5468f9321ea45ff0e96d6babe35c628f5576ddad
hank <huanghank2000@gmail.com>
2023-08-08 10:56:55 +0800

    Rock-Paper-Scissors

--- Day4/Rock-paper-scissors.py
+++ Day4/Rock-paper-scissors.py
@@ -3,41 +3,83 @@
 # 1 > 0, 1 < 2, 1 = 1 
 # 2 > 1, 2 < 0, 2 = 2 
 
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
 import random
 
 my_choose = int(input('What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.'))
 print(my_choose)
+if my_choose == 0:
+    print(rock)
+elif my_choose == 1:
+    print(paper)
+elif my_choose == 2:
+    print(scissors)
+else:
+    print("please try again") 
 
 com_choose = random.randint(0,2)
 print(com_choose)
+if com_choose == 0:
+    print(rock)
+elif com_choose == 1:
+    print(paper)
+elif com_choose == 2:
+    print(scissors)
+else:
+    print("please try again") 
 
 
 # I am 0
-if my_choose == 0 and com_choose == 2:
-    print("You win")
-elif my_choose == 0 and com_choose == 1:
-    print("You lose")
-elif my_choose == com_choose:
-    print("Draw\nPlease try again")
-else:
-    print("please try again")
-
-# I am 1
-if my_choose == 1 and com_choose == 0:
-    print("You win")
-elif my_choose == 1 and com_choose == 2:
-    print("You lose")
-elif my_choose == com_choose:
-    print("Draw\nPlease try again")
-else:
-    print("please try again")
-
-# I am 2
-if my_choose == 2 and com_choose == 1:
-    print("You win")
-elif my_choose == 2 and com_choose == 0:
-    print("You lose")
-elif my_choose == com_choose:
-    print("Draw\nPlease try again")
-else:
-    print("please try again")
+if my_choose == 0:
+    if com_choose == 2:
+        print("You win")
+    elif com_choose == 1:
+        print("You lose")
+    elif my_choose == com_choose:
+        print("Draw\nPlease try again")
+    else:
+        print("please try again")
+elif my_choose == 1 :
+    if com_choose == 0:
+        print("You win")
+    elif com_choose == 2:
+        print("You lose")
+    elif my_choose == com_choose:
+        print("Draw\nPlease try again")
+    else:
+        print("please try again")
+elif my_choose == 2:
+    if com_choose == 1:
+        print("You win")
+    elif com_choose == 0:
+        print("You lose")
+    elif my_choose == com_choose:
+        print("Draw\nPlease try again")
+    else:
+        print("please try again")
